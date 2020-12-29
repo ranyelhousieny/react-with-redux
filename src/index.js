326a59dacc298b5e04953451976f768bb399c6a7
Rany Elhousieny <elhousie@gmail.com>
2020-12-28 16:36:01 -0800

    2020 Dec 28

--- src/index.js
+++ src/index.js
@@ -9,5 +9,9 @@ import { createStore } from 'redux';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById( 'root' ) );
 
